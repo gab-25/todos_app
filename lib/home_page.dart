73169e0dd574b42e278773710a3ebf785013aa0b
Gab25 <gabrielesorci.25@gmail.com>
2025-02-22 11:41:59 +0100

    add login

---
+++ lib/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:energy_monitor_app/main.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text(appTitle),
+      ),
+    );
+  }
+}
