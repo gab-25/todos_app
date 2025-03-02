5eb27101c0c667f152baec3a469bcbd6714d0d94
Gab25 <gabrielesorci.25@gmail.com>
2025-03-02 12:09:41 +0100

    .

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:energy_monitor_app/blocs/app/app_bloc.dart';
 import 'package:energy_monitor_app/blocs/app/app_event.dart';
+import 'package:energy_monitor_app/blocs/app/app_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -8,38 +9,43 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: const Text('Monitor'),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.account_circle_outlined),
-              onPressed: () {},
-            ),
-            IconButton(
-              icon: const Icon(Icons.logout),
-              onPressed: () =>
-                  context.read<AppBloc>().add(const AppLogoutPressed()),
-            ),
-          ],
-        ),
-        body: const TabBarView(
-          children: [
-            Center(child: Text('Monitor')),
-            Center(child: Text('Statistics')),
-            Center(child: Text('Settings')),
-          ],
-        ),
-        bottomNavigationBar: const TabBar(
-          tabs: [
-            Tab(icon: Icon(Icons.monitor)),
-            Tab(icon: Icon(Icons.bar_chart_outlined)),
-            Tab(icon: Icon(Icons.settings)),
-          ],
-        ),
+    final AppTabs tabSelected = context.select((AppBloc bloc) => bloc.state.tabSelected);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(tabSelected.toString()),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.account_circle_outlined),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.logout_outlined),
+            onPressed: () =>
+                context.read<AppBloc>().add(const AppLogoutPressed()),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.monitor_outlined),
+            label: AppTabs.monitor.toString(),
+          ),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.bar_chart_outlined),
+            label: AppTabs.statistics.toString(),
+          ),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.settings_outlined),
+            label: AppTabs.settings.toString(),
+          ),
+        ],
+        onTap: (value) => context.read<AppBloc>().add(AppTabPressed(AppTabs.values[value])),
+        currentIndex: AppTabs.values.indexOf(tabSelected),
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
     );
   }
