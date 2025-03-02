855fbc44c7bd35deb516c0bcc1d8d5eee823a47d
Gab25 <gabrielesorci.25@gmail.com>
2025-03-02 12:12:05 +0100

    add all pages

---
+++ lib/ui/pages/monitor_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MonitorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Monitor'),
+      ),
+      body: Center(
+        child: Text('Monitor Page'),
+      ),
+    );
+  }
+}
