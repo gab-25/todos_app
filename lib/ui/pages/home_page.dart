630dff3f3312500a0f15f1a078c13c614f03d8e8
Gab25 <gabrielesorci.25@gmail.com>
2025-03-02 10:47:51 +0100

    add tabs

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -8,21 +8,38 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final user = context.select((AppBloc bloc) => bloc.state.user);
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Monitor'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.logout),
-            onPressed: () =>
-                context.read<AppBloc>().add(const AppLogoutPressed()),
-          ),
-        ],
-      ),
-      body: Center(
-        child: Text(user?.email ?? 'No user'),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('Monitor'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.account_circle_outlined),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: const Icon(Icons.logout),
+              onPressed: () =>
+                  context.read<AppBloc>().add(const AppLogoutPressed()),
+            ),
+          ],
+        ),
+        body: const TabBarView(
+          children: [
+            Center(child: Text('Monitor')),
+            Center(child: Text('Statistics')),
+            Center(child: Text('Settings')),
+          ],
+        ),
+        bottomNavigationBar: const TabBar(
+          tabs: [
+            Tab(icon: Icon(Icons.monitor)),
+            Tab(icon: Icon(Icons.bar_chart_outlined)),
+            Tab(icon: Icon(Icons.settings)),
+          ],
+        ),
       ),
     );
   }
