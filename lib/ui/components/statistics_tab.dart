5f6fb584f5a63af7e082c83c82bd4a86a324bf4d
Gab25 <gabrielesorci.25@gmail.com>
2025-03-14 16:45:24 +0100

    ini statistics page

--- lib/ui/components/statistics_tab.dart
+++ lib/ui/components/statistics_tab.dart
@@ -1,12 +1,28 @@
+import 'package:energy_monitor_app/cubits/statistics/statistics_cubit.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class StatisticsTab extends StatelessWidget {
   const StatisticsTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Statistics Page'),
+    return BlocProvider(
+      create: (context) => StatisticsCubit(),
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: BlocBuilder<StatisticsCubit, StatisticsState>(
+          builder: (context, state) => Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              BarChart(
+                BarChartData(),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
